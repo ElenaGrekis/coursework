44747f705898eee5522878f73f3f75e4f59dec76
Elena Grekis <elena_belladonna@mail.ru>
2013-12-18 00:55:40 +0300

    fix

--- 1_poll/mysite/urls.py
+++ 1_poll/mysite/urls.py
@@ -4,7 +4,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
     # Examples:
